1e9086241ee9705cbe20d3cea04ec0111b55348e
Faizal Ismail <muhammad.faizal@finexusgroup.com>
2025-05-05 13:14:25 +0800

    add gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'faizalizm.github.io',
+  base: '/faizalizm.github.io/',
   plugins: [react()],
 })
